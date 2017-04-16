d8686561dba0d79cb7ae63fe05d48a1051023561
troywith77 <ruitang307@gmail.com>
2017-04-16 16:18:26 +0800

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: './index.js',
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{
+			test: /(scss)$/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: ['css-loader', 'sass-loader', {
+					loader: 'postcss-loader',
+					options: {
+						plugins: [require('autoprefixer')]
+					}
+				}]
+			})
+		}]
+	},
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ]
+}
